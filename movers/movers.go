5c1d74ed6fa01299a2d30346fe198578afe3750f
sam <samuel.troyer.j@gmail.com>
2022-05-10 16:15:44 -0600

    movers package working again

--- movers/movers.go
+++ movers/movers.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	. "github.com/samjtro/go-tda/utils"
 )
+ 
+var endpoint_movers string = "https://api.tdameritrade.com/v1/marketdata/%s/movers"// 	 		--> index
 
 // Get returns a string; containing information on the desired index's movers per your desired direction and change type(percent or value), 
 // it takes three parameters:
