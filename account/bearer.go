98d5c3fb48fd07d7c36fae18af8994b247a42644
sam <samuel.troyer.j@gmail.com>
2022-05-12 08:55:57 -0600

    gofmt, pushing to v0.5.5

--- account/bearer.go
+++ account/bearer.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 	. "github.com/samjtro/go-tda/utils"
 )
+
+var endpoint = "https://api.tdameritrade.com/v1/oauth2/token"
+
 // GetBearerToken returns a string; containing the Bearer Token for your account
-func GetBearerToken(code,client_id,redirect_url string) {}
+func GetBearerToken(code,client_id,redirect_url string) {
+	
+}
 
