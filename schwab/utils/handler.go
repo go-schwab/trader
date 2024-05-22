5a55897d7b8a08bf82eea173ce73170824a45f89
sam <sam@rhizome.rhizome.com>
2024-05-21 23:31:18 -0500

    Major - First attempt at Schwab oAuth Flow. Too tired for testing tonight, will finish up tomorrow but I believe the foundation is there.

--- schwab/utils/handler.go
+++ schwab/utils/handler.go
@@ -4,8 +4,69 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"sync"
+	"fmt"
+	"strings"
+	"bytes"
+	"encoding/base64"
 )
+
+type Token struct {
+	refresh string `json:"refresh_token"` 
+	access string `json:"access_token"`
+}
+
+func init() {
+	config, err := LoadConfig()
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+// Credit: https://stackoverflow.com/questions/26916952/go-retrieve-a-string-from-between-two-characters-or-other-strings
+func GetStringInBetween(str string, start string, end string) (result string) {     
+	s := strings.Index(str, start)
+	s += len(start)
+	e := strings.Index(str[s:], end)
+	e += s + e - 1
+
+	return str[s:e] 
+}
+
+func oAuth() {
+	// Get Auth Code
+	var (
+		m sync.Mutex
+	)
+
+	resp, err := http.Get(fmt.Sprintf("https://api.schwabapi.com/v1/oauth/authorize?client_id=%s&redirect_uri=127.0.0.1", config.APPKEY))
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	} else if resp.StatusCode != 404 {
+		log.Fatalf(err.Error())
+	}
+
+	authCodeEncoded := GetStringInBetween(resp.Request.URL.String(), "?code=", "&session=")
+
+	// POST Request for Bearer/Refresh Tokens
+	EncodedIDSecret := base64.URLEncoding.EncodeToString(fmt.Sprintf("%s:%s", config.APPKEY, config.SECRET)
+	client := http.Client{}
+	req, err := http.NewRequest("POST", "https://api.schwabapi.com/v1/oauth/token", bytes.NewBuffer(fmt.Sprintf("grant_type=authorization_code&code=%s&redirect_uri=https://example_url.com/callback_example"), url.QueryUnescape(authCodeEncoded)))
+	req.Header = http.Header{
+		"Content-Type": {"application/x-www-form-urlencoded"},
+		"Authorization": {EncodedIDSecret},
+	}
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	res = res.(Tokens)
+}
 
 // Handler is the general purpose request function for the td-ameritrade api, all functions will be routed through this handler function, which does all of the API calling work
 // It performs a GET request after adding the apikey found in the .APIKEY file in the same directory as the program calling the function,
@@ -19,14 +80,7 @@ func Handler(req *http.Request) (string, error) {
 
 	m.Lock()
 
-	config, err := LoadConfig()
-
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
 	q := req.URL.Query()
-	// Need to add config.APPKEY & config.SECRET to header to authenticate Market Data Production requests
 	req.URL.RawQuery = q.Encode()
 	client := http.Client{}
 	resp, err := client.Do(req)
