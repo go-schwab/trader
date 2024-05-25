0488f919afe3892f3911de1a4d049734f9a87e0c
sam <samuel.troyer.j@gmail.com>
2024-05-25 13:52:15 -0500

    Major - v0.8.6.4.8

--- schwab/utils/handler.go
+++ schwab/utils/handler.go
@@ -131,9 +131,8 @@ func oAuthInit() TOKEN {
 	}
 
 	// oAuth Leg 2 - Access Token Creation
-	EncodedIDSecret := url.QueryEscape(fmt.Sprintf("Basic %s:%s", config.APPKEY, config.SECRET))
+	EncodedIDSecret := base64.StdEncoding.EncodeToString([]byte(url.QueryEscape(fmt.Sprintf("%s:%s", config.APPKEY, config.SECRET))))
 	client := http.Client{}
-	fmt.Println(authCodeDecoded)
 	req, err := http.NewRequest("POST", "https://api.schwabapi.com/v1/oauth/token", bytes.NewBuffer([]byte(fmt.Sprintf("grant_type=authorization_code&code=%s&redirect_uri=https://example_url.com/callback_example", authCodeDecoded))))
 
 	if err != nil {
