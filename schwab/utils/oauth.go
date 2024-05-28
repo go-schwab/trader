a417ee6b5852879320c1ccc5cb4559fd40c1d60e
samjtro <hi@samjtro.com>
2024-05-28 13:11:57 -0500

    Testing

--- schwab/utils/oauth.go
+++ schwab/utils/oauth.go
@@ -63,7 +63,7 @@ func oAuthInit() TOKEN {
 	tokensJson, err := json.Marshal(tokens)
 	utils.Check(err)
 
-	f, err := os.Create("~/.foo/bar.json")
+	f, err := os.OpenFile("~/.foo/bar.json", os.O_CREATE, 0666)
 	utils.Check(err)
 	defer f.Close()
 	_, err = f.Write(tokensJson)
