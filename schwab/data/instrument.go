7d4fd80a4dbf59a3392c86403181d65df0749e3a
samjtro <hi@samjtro.com>
2024-06-02 08:16:05 -0500

    testing

--- schwab/data/instrument.go
+++ schwab/data/instrument.go
@@ -1,19 +1,23 @@
 package data
 
+import (
+	"net/http"
+	"strings"
+
+	schwabutils "github.com/samjtro/go-trade/schwab/utils"
+	utils "github.com/samjtro/go-trade/utils"
+)
+
 // Search for an instrument by a specific symbol, return all fundamental asset information.
 func SearchInstrument(symbol string) (INSTRUMENT, error) {
-	/*req, _ := http.NewRequest("GET", Endpoint_searchinstruments, nil)
+	req, _ := http.NewRequest("GET", Endpoint_searchinstruments, nil)
 	q := req.URL.Query()
 	q.Add("symbol", symbol)
 	q.Add("projection", "fundamental")
 	req.URL.RawQuery = q.Encode()
-	body, err := schwabutils.Handler(req) // schwabutils "github.com/samjtro/go-trade/schwab/utils"
+	body, err := schwabutils.Handler(req)
+	utils.Check(err)
 
-	if err != nil {
-		return INSTRUMENT{}, err
-	}
-
-	split := strings.Split(body, "{")*/
-
+	_ = strings.Split(body, "{")
 	return INSTRUMENT{}, nil
 }
