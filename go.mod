25029927194174df7e64aeab902015377674c2d2
samjtro <hi@samjtro.com>
2024-10-25 10:11:08 -0500

    major: ref: #67, v0.9.0 desc: - refactor utils.go to reflect removal of previous oauth implementation in favor of x/oauth2, vis-a-vis go-schwab/oauth2ns - check -> isErrNil

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/go-schwab/trader
 
-go 1.22.4
+go 1.23.2
 
 require github.com/joho/godotenv v1.5.1
 
@@ -9,7 +9,16 @@ require (
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/go-schwab/oauth2ns v0.0.0-20241015193425-e8abfd05a439 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/nmrshll/rndm-go v0.0.0-20170430161430-8da3024e53de // indirect
+	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- utils.go
+++ utils.go
@@ -1,66 +1,65 @@
 package trader
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"os/exec"
-	"runtime"
 	"strings"
 	"time"
 
+	"github.com/bytedance/sonic"
+	"github.com/go-schwab/oauth2ns"
+	o "github.com/go-schwab/oauth2ns"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
+type Agent struct{ client *o.AuthorizedClient }
+
+type DB struct {
+	AccessToken  string
+	RefreshToken string
+	TokenType    string
+	Expiry       time.Time
+	ExpiresIn    int64
+}
+
+var Tokens DB
+
 func init() {
-	err := godotenv.Load("config.env")
-	check(err)
-}
-
-type Agent struct {
-	tokens Token
-}
-
-type Token struct {
-	RefreshExpiration time.Time
-	Refresh           string
-	BearerExpiration  time.Time
-	Bearer            string
-}
-
-// Helper: parse access token response
-func parseAccessTokenResponse(s string) Token {
-	token := Token{
-		RefreshExpiration: time.Now().Add(time.Hour * 168),
-		BearerExpiration:  time.Now().Add(time.Minute * 30),
-	}
-	for _, x := range strings.Split(s, ",") {
-		for i1, x1 := range strings.Split(x, ":") {
-			if trimOneFirstOneLast(x1) == "refresh_token" {
-				token.Refresh = trimOneFirstOneLast(strings.Split(x, ":")[i1+1])
-			} else if trimOneFirstOneLast(x1) == "access_token" {
-				token.Bearer = trimOneFirstOneLast(strings.Split(x, ":")[i1+1])
-			}
-		}
-	}
-	return token
+	err := godotenv.Load("*.env")
+	isErrNil(err)
 }
 
 // Read in tokens from ~/.trade/bar.json
-func readDB() Token {
-	var tokens Token
+func readDB() *oauth2ns.AuthorizedClient {
 	body, err := os.ReadFile(fmt.Sprintf("%s/.trade/bar.json", homeDir()))
-	check(err)
-	err = json.Unmarshal(body, &tokens)
-	check(err)
-	return tokens
+	isErrNil(err)
+	var ctx context.Context
+	err = sonic.Unmarshal(body, &Tokens)
+	isErrNil(err)
+	token := new(oauth2.Token)
+	token.AccessToken = Tokens.AccessToken
+	token.RefreshToken = Tokens.RefreshToken
+	token.TokenType = Tokens.TokenType
+	token.Expiry = Tokens.Expiry
+	token.ExpiresIn = Tokens.ExpiresIn
+	c := &oauth2.Config{
+		ClientID:     os.Getenv("APPKEY"),
+		ClientSecret: os.Getenv("SECRET"),
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://api.schwabapi.com/v1/oauth/authorize",
+			TokenURL: "https://api.schwabapi.com/v1/oauth/token",
+		},
+	}
+	return &o.AuthorizedClient{
+		c.Client(ctx, token),
+		token,
+	}
 }
 
 // Credit: https://go.dev/play/p/C2sZRYC15XN
@@ -77,24 +76,8 @@ func getStringInBetween(str string, start string, end string) (result string) {
 	return str[s : s+e]
 }
 
-// Credit: https://gist.github.com/hyg/9c4afcd91fe24316cbf0
-func openBrowser(url string) {
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		log.Fatalf("Unsupported platform.")
-	}
-	check(err)
-}
-
-// Generic error checking, will be implementing more robust error/exception handling >v0.9.0
-func check(err error) {
+// is the err nil?
+func isErrNil(err error) {
 	if err != nil {
 		log.Fatalf("[ERR] %s", err.Error())
 	}
@@ -151,72 +134,34 @@ func trimOneFirstThreeLast(s string) string {
 // wrapper for os.UserHomeDir()
 func homeDir() string {
 	dir, err := os.UserHomeDir()
-	check(err)
+	isErrNil(err)
 	return dir
 }
 
-// Initiate the Schwab oAuth process to retrieve bearer/refresh tokens
 func Initiate() *Agent {
-	agent := Agent{}
+	var agent Agent
 	if _, err := os.Stat(fmt.Sprintf("%s/.trade", homeDir())); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(fmt.Sprintf("%s/.trade", homeDir()), os.ModePerm)
-		check(err)
-		// oAuth Leg 1 - Authorization Code
-		openBrowser(fmt.Sprintf("https://api.schwabapi.com/v1/oauth/authorize?client_id=%s&redirect_uri=%s", os.Getenv("APPKEY"), os.Getenv("CBURL")))
-		fmt.Printf("Log into your Schwab brokerage account. Copy Error404 URL and paste it here: ")
-		var urlInput string
-		fmt.Scanln(&urlInput)
-		authCodeEncoded := getStringInBetween(urlInput, "?code=", "&session=")
-		authCode, err := url.QueryUnescape(authCodeEncoded)
-		check(err)
-		// oAuth Leg 2 - Refresh, Bearer Tokens
-		authStringLegTwo := fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", os.Getenv("APPKEY"), os.Getenv("SECRET")))))
-		client := http.Client{}
-		payload := fmt.Sprintf("grant_type=authorization_code&code=%s&redirect_uri=%s", string(authCode), os.Getenv("CBURL"))
-		req, err := http.NewRequest("POST", "https://api.schwabapi.com/v1/oauth/token", bytes.NewBuffer([]byte(payload)))
-		check(err)
-		req.Header = http.Header{
-			"Authorization": {authStringLegTwo},
-			"Content-Type":  {"application/x-www-form-urlencoded"},
-		}
-		res, err := client.Do(req)
-		check(err)
-		defer res.Body.Close()
-		bodyBytes, err := io.ReadAll(res.Body)
-		check(err)
-		agent.tokens = parseAccessTokenResponse(string(bodyBytes))
-		tokensJson, err := json.Marshal(agent.tokens)
-		check(err)
-		err = os.WriteFile(fmt.Sprintf("%s/.trade/bar.json", homeDir()), tokensJson, 0777)
-		check(err)
+		err = os.Mkdir(fmt.Sprintf("%s/.trade", homeDir()), os.ModePerm)
+		isErrNil(err)
+		agent.client, err = o.Run()
+		isErrNil(err)
+		Tokens.AccessToken = agent.client.Token.AccessToken
+		Tokens.RefreshToken = agent.client.Token.RefreshToken
+		Tokens.TokenType = agent.client.Token.TokenType
+		Tokens.Expiry = agent.client.Token.Expiry
+		Tokens.ExpiresIn = agent.client.Token.ExpiresIn
+		bytes, err := sonic.Marshal(Tokens)
+		err = os.WriteFile(fmt.Sprintf("%s/.trade/bar.json", homeDir()), bytes, 0777)
+		isErrNil(err)
 	} else {
-		agent.tokens = readDB()
-		if agent.tokens.Bearer == "" {
+		agent.client = readDB()
+		if Tokens.AccessToken == "" {
 			err := os.RemoveAll(fmt.Sprintf("%s/.trade", homeDir()))
-			check(err)
-			log.Fatalf("[err] please reinitiate, something went wrong\n")
+			isErrNil(err)
+			log.Fatalf("[err] something went wrong - please reinitiate with 'Initiate'")
 		}
 	}
 	return &agent
-}
-
-// Use refresh token to generate a new bearer token for authentication
-func (agent *Agent) refresh() {
-	oldTokens := readDB()
-	authStringRefresh := fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", os.Getenv("APPKEY"), os.Getenv("SECRET")))))
-	client := http.Client{}
-	req, err := http.NewRequest("POST", "https://api.schwabapi.com/v1/oauth/token", bytes.NewBuffer([]byte(fmt.Sprintf("grant_type=refresh_token&refresh_token=%s", oldTokens.Refresh))))
-	check(err)
-	req.Header = http.Header{
-		"Authorization": {authStringRefresh},
-		"Content-Type":  {"application/x-www-form-urlencoded"},
-	}
-	res, err := client.Do(req)
-	check(err)
-	defer res.Body.Close()
-	bodyBytes, err := io.ReadAll(res.Body)
-	check(err)
-	agent.tokens = parseAccessTokenResponse(string(bodyBytes))
 }
 
 // Handler is the general purpose request function for the td-ameritrade api, all functions will be routed through this handler function, which does all of the API calling work
@@ -225,27 +170,37 @@ func (agent *Agent) refresh() {
 // It takes one parameter:
 // req = a request of type *http.Request
 func (agent *Agent) Handler(req *http.Request) (*http.Response, error) {
-	if (&Agent{}) == agent {
-		log.Fatal("[ERR] empty agent - call 'Agent.Initiate' before making any API function calls.")
+	var err error
+	if Tokens.AccessToken == "" {
+		log.Fatalf("[err] no access token found, please reinitiate with 'Initiate'")
 	}
-	if !time.Now().Before(agent.tokens.BearerExpiration) {
-		agent.refresh()
+	if ((&Agent{}) == agent) || ((&o.AuthorizedClient{}) == agent.client) {
+		agent.client, err = o.Run()
+		isErrNil(err)
+		Tokens.AccessToken = agent.client.Token.AccessToken
+		Tokens.RefreshToken = agent.client.Token.RefreshToken
+		Tokens.TokenType = agent.client.Token.TokenType
+		Tokens.Expiry = agent.client.Token.Expiry
+		Tokens.ExpiresIn = agent.client.Token.ExpiresIn
 	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", agent.tokens.Bearer))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", agent.client.Token.AccessToken))
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return resp, err
 	}
-	if resp.StatusCode == 401 {
-		err := os.Remove(fmt.Sprintf("%s/.trade", homeDir()))
-		check(err)
-	}
-	if resp.StatusCode < 200 || resp.StatusCode > 300 {
+	// TODO: test this block
+	var statusErr error
+	switch true {
+	case resp.StatusCode == 401:
+		err := os.RemoveAll(fmt.Sprintf("%s/.trade", homeDir()))
+		isErrNil(err)
+		statusErr = errors.New("[err] invalid token - please reinitiate with 'Initiate'")
+	case resp.StatusCode < 200, resp.StatusCode > 300:
 		defer resp.Body.Close()
 		body, err := io.ReadAll(resp.Body)
-		check(err)
-		log.Fatalf("[ERR] %d, %s", resp.StatusCode, body)
+		isErrNil(err)
+		statusErr = errors.New(fmt.Sprintf("[err] %d - %s", resp.StatusCode, body))
 	}
-	return resp, nil
+	return resp, statusErr
 }
