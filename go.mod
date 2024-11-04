95fbe3a5b7b19f9da193994482840c2fa57df2a4
samjtro <hi@samjtro.com>
2024-11-03 19:02:08 -0600

    major: ref: #67, v0.9.0 desc: - finally got x/oauth2 working - moved token storage into cwd/.json - refactored utils.go, removed unused funcs - edited README, gitignore to reflect changes

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.2
 
 require (
 	github.com/bytedance/sonic v1.12.2
-	github.com/go-schwab/oauth2ns v0.0.0-20241027015119-9e1292705f91
+	github.com/go-schwab/utils/oauth v0.0.0-20241103230919-01b09562dfc2
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/oauth2 v0.23.0
 )
@@ -13,14 +13,12 @@ require (
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
-	github.com/fatih/color v1.17.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/nmrshll/rndm-go v0.0.0-20170430161430-8da3024e53de // indirect
-	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177 // indirect
-	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- utils.go
+++ utils.go
@@ -2,8 +2,8 @@ package trader
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
-	"fmt"
 	"io"
 	"io/fs"
 	"log"
@@ -13,18 +13,25 @@ import (
 	"strings"
 
 	"github.com/bytedance/sonic"
-	o "github.com/go-schwab/oauth2ns"
+	o "github.com/go-schwab/utils/oauth"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 )
 
-type Agent struct{ client *o.AuthorizedClient }
+type Agent struct{ *o.AuthorizedClient }
 
-var TOKENS oauth2.Token
+var (
+	APPKEY string
+	SECRET string
+	CBURL  string
+)
 
 func init() {
 	err := godotenv.Load(findAllEnvFiles()...)
 	isErrNil(err)
+	APPKEY = os.Getenv("APPKEY")
+	SECRET = os.Getenv("SECRET")
+	CBURL = os.Getenv("CBURL")
 }
 
 // is the err nil?
@@ -53,119 +60,45 @@ func findAllEnvFiles() []string {
 	return files
 }
 
-// wrapper for os.UserHomeDir()
-func homeDir() string {
-	dir, err := os.UserHomeDir()
+// Read in tokens from .json
+func readDB() Agent {
+	var tok *oauth2.Token
+	body, err := os.ReadFile(".json")
 	isErrNil(err)
-	return dir
-}
-
-// Credit: https://go.dev/play/p/C2sZRYC15XN
-func getStringInBetween(str string, start string, end string) (result string) {
-	s := strings.Index(str, start)
-	if s == -1 {
-		return
-	}
-	s += len(start)
-	e := strings.Index(str[s:], end)
-	if e == -1 {
-		return
-	}
-	return str[s : s+e]
-}
-
-// trim one FIRST character in the string
-func trimOneFirst(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[1:]
-}
-
-// trim one LAST character in the string
-func trimOneLast(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[:len(s)-1]
-}
-
-// trim one FIRST & one LAST character in the string
-func trimOneFirstOneLast(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[1 : len(s)-1]
-}
-
-// trim two FIRST & one LAST character in the string
-func trimTwoFirstOneLast(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[2 : len(s)-1]
-}
-
-// trim one FIRST & two LAST character in the string
-func trimOneFirstTwoLast(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[1 : len(s)-2]
-}
-
-// trim one FIRST & three LAST character in the string
-func trimOneFirstThreeLast(s string) string {
-	if len(s) < 1 {
-		return ""
-	}
-	return s[1 : len(s)-3]
-}
-
-// Read in tokens from ~/.trade/bar.json
-func readDB() *o.AuthorizedClient {
-	body, err := os.ReadFile(fmt.Sprintf("%s/.trade/bar.json", homeDir()))
+	err = sonic.Unmarshal(body, &tok)
 	isErrNil(err)
-	var ctx context.Context
-	err = sonic.Unmarshal(body, &TOKENS)
-	isErrNil(err)
-	c := &oauth2.Config{
-		ClientID:     os.Getenv("APPKEY"),
-		ClientSecret: os.Getenv("SECRET"),
+	conf := &oauth2.Config{
+		ClientID:     APPKEY, // Schwab App Key
+		ClientSecret: SECRET, // Schwab App Secret
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://api.schwabapi.com/v1/oauth/authorize",
 			TokenURL: "https://api.schwabapi.com/v1/oauth/token",
 		},
 	}
-	return &o.AuthorizedClient{
-		c.Client(ctx, &TOKENS),
-		&TOKENS,
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	sslcli := &http.Client{Transport: tr}
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, sslcli)
+	return Agent{
+		&o.AuthorizedClient{
+			conf.Client(ctx, tok),
+			tok,
+		},
 	}
 }
 
 func Initiate() *Agent {
 	var agent Agent
 	// TODO: test this block, this is to attempt to resolve the error described in #67
-	if _, err := os.Stat(fmt.Sprintf("%s/.trade/bar.json", homeDir())); errors.Is(err, os.ErrNotExist) {
-		if _, err := os.Stat(fmt.Sprintf("%s/.trade", homeDir())); !errors.Is(err, os.ErrNotExist) {
-			err = os.RemoveAll(fmt.Sprintf("%s/.trade", homeDir()))
-			isErrNil(err)
-		}
-		err = os.Mkdir(fmt.Sprintf("%s/.trade", homeDir()), os.ModePerm)
+	if _, err := os.Stat(".json"); errors.Is(err, os.ErrNotExist) {
+		agent = Agent{o.Initiate(APPKEY, SECRET)}
+		bytes, err := sonic.Marshal(agent.Token)
 		isErrNil(err)
-		agent.client, err = o.Initiate(os.Getenv("APPKEY"), os.Getenv("SECRET"), os.Getenv("CBURL"))
-		isErrNil(err)
-		TOKENS = *agent.client.Token
-		bytes, err := sonic.Marshal(TOKENS)
-		err = os.WriteFile(fmt.Sprintf("%s/.trade/bar.json", homeDir()), bytes, 0777)
+		err = os.WriteFile(".json", bytes, 0777)
 		isErrNil(err)
 	} else {
-		agent.client = readDB()
-		if TOKENS.AccessToken == "" {
-			err := os.RemoveAll(fmt.Sprintf("%s/.trade", homeDir()))
-			isErrNil(err)
-			log.Fatalf("[err] something went wrong - please reinitiate with 'Initiate'")
-		}
+		agent = readDB()
 	}
 	return &agent
 }
@@ -177,32 +110,22 @@ func Initiate() *Agent {
 // req = a request of type *http.Request
 func (agent *Agent) Handler(req *http.Request) (*http.Response, error) {
 	var err error
-	if TOKENS.AccessToken == "" {
-		log.Fatalf("[err] no access token found, please reinitiate with 'Initiate'")
+	if agent.Token.AccessToken == "" {
+		log.Fatal("[fatal] no access token found, please reinitiate with 'Initiate'")
+		// TODO: auto reinitiate?
 	}
-	if ((&Agent{}) == agent) || ((&o.AuthorizedClient{}) == agent.client) {
-		// TODO: this theoretically works but results in an error for the oauth implementation
-		// going to do some testing now, but pushing as it is in what is a theoretically working state
-		agent.client, err = o.Initiate(os.Getenv("APPKEY"), os.Getenv("SECRET"), os.Getenv("CBURL"))
-		isErrNil(err)
-		TOKENS = *agent.client.Token
-	}
-	resp, err := agent.client.Do(req)
+	resp, err := agent.Do(req)
 	if err != nil {
 		return resp, err
 	}
-	// TODO: test this block
-	var statusErr error
 	switch true {
 	case resp.StatusCode == 401:
-		err := os.RemoveAll(fmt.Sprintf("%s/.trade", homeDir()))
-		isErrNil(err)
-		statusErr = errors.New("[err] invalid token - please reinitiate with 'Initiate'")
+		log.Fatal("[fatal] invalid token - please reinitiate with 'Initiate'")
 	case resp.StatusCode < 200, resp.StatusCode > 300:
 		defer resp.Body.Close()
 		body, err := io.ReadAll(resp.Body)
 		isErrNil(err)
-		statusErr = errors.New(fmt.Sprintf("[err] %d - %s", resp.StatusCode, body))
+		log.Println("[err] ", string(body))
 	}
-	return resp, statusErr
+	return resp, nil
 }
